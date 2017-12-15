04999db35426108d7ab863fe7a96d87149470380
Liang Zhang <zhangliangnuaa@gmail.com>
2017-12-15 12:06:23 +0800

    Add 'add new todo' function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,4 @@
-$('li').on("click", function () {
+$('ul').on("click", "li", function () {
 	// if ($(this).css("color") == "rgb(128, 128, 128)") {
 	// 	$(this).css({
 	// 		color: "black",
@@ -11,4 +11,21 @@ $('li').on("click", function () {
 	// 	});
 	// }
 	$(this).toggleClass("completed")
+});
+
+
+$('ul').on("click", "span", function (event) {
+	$(this).parent().fadeOut(500, function () {
+		$(this).remove();
+	});
+
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
